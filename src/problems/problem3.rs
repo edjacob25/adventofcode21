b1ec2c3e3583b68c9dcf37335a04ff70ec0235dd
Jacob Rivera <edjacob25@gmail.com>
2021-12-04 16:46:39 -0600

    Fixed day 3 solution

--- src/problems/problem3.rs
+++ src/problems/problem3.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 use super::common::read_file;
 
-pub fn do_something() {
+pub fn part1() {
     let path = "test_files/3.txt".to_string();
     let lines = read_file(path);
-    let mut counts: [u16; 12] = [0; 12];
+    let mut counts = vec![0; lines[0].len()];
     let size = lines.len();
     for line in lines {
         let split: Vec<&str> = line.split_terminator("").skip(1).collect();
@@ -12,10 +14,11 @@ pub fn do_something() {
             counts[i] += n;
         }
     }
-    let gamma_arr: [u8; 12] = create_bit_array(&counts, size);
-    let epsilon_arr = invert_bit_array(&gamma_arr);
-    let gamma = convert_array_bit_to_int(&gamma_arr);
-    let epsilon = convert_array_bit_to_int(&epsilon_arr);
+    let gamma = create_bit_array(&counts, size);
+    println!("{:?}", gamma);
+    let epsilon = invert_bit_array(&gamma);
+    let gamma = convert_array_bit_to_int(&gamma);
+    let epsilon = convert_array_bit_to_int(&epsilon);
     println!("Gamma is {} and epsilon is {}", gamma, epsilon);
 
 
@@ -24,17 +27,17 @@ pub fn do_something() {
     println!("The power consumption is {}", power_consumption);
 }
 
-fn create_bit_array(counts: &[u16; 12], len: usize) -> [u8; 12] {
-    let mut bit_array = [0;12];
+fn create_bit_array(counts: &[u16], len: usize) -> Vec<u8> {
+    let mut bit_array = vec![0; counts.len()];
     for (i, count) in counts.iter().enumerate() {
-        bit_array[i] =  (*count > (len / 2) as u16) as u8;
+        bit_array[i] = (*count >= (len / 2) as u16) as u8;
     }
     bit_array
     // counts.iter().map(|x| (*x > (len / 2) as u16) as u8).collect()
 }
 
-fn invert_bit_array(array: &[u8; 12]) -> [u8; 12] {
-    let mut new_arr = [0; 12];
+fn invert_bit_array(array: &[u8]) -> Vec<u8> {
+    let mut new_arr = vec![0; array.len()];
     for (i, u) in array.iter().enumerate() {
         if *u == 0 as u8 {
             new_arr[i] = 1;
@@ -43,9 +46,14 @@ fn invert_bit_array(array: &[u8; 12]) -> [u8; 12] {
     new_arr
 }
 
-fn convert_array_bit_to_int(bits: &[u8; 12]) -> u32 {
+fn convert_array_bit_to_int(bits: &[u8]) -> u32 {
     bits.iter()
         .fold(0, |result, &bit| {
             (result << 1) ^ bit as u32
         })
+}
+
+enum LookingFor {
+    MIN,
+    MAX,
 }
