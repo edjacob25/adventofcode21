6ea9319dae3f60390e4918b5ec437f0c726f4419
Jacob Rivera <edjacob25@gmail.com>
2021-12-04 16:25:24 -0600

    Added second part of day 2

--- src/problems/problem2.rs
+++ src/problems/problem2.rs
@@ -1,6 +1,6 @@
 use super::common::read_file;
 
-pub fn do_something() {
+pub fn part1() {
     let path = "test_files/2.txt".to_string();
     let lines = read_file(path);
     let mut x: u32 = 0;
@@ -17,5 +17,29 @@ pub fn do_something() {
     }
     let result = x * y;
     println!("Dec 2");
+    println!("The resulting number is {}", result);
+}
+
+pub fn part2() {
+    let path = "test_files/2.txt".to_string();
+    let lines = read_file(path);
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+    for line in lines {
+        let split: Vec<&str> = line.split_whitespace().collect();
+        let quantity = split[1].parse::<u32>().unwrap();
+        match split[0] {
+            "forward" => {
+                x += quantity;
+                y += aim * quantity;
+            }
+            "up" => { aim -= quantity }
+            "down" => { aim += quantity }
+            _ => ()
+        }
+    }
+    let result = x * y;
+    println!("Dec 2, part 2");
     println!("The resulting number is {}", result);
 }
