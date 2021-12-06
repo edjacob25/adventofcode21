7df43de3c207e59c17bd29154d5f95c4a7d8cdb4
Jacob Rivera <edjacob25@gmail.com>
2021-12-05 18:27:18 -0600

    Added solution to second problem day 5

--- src/problems/problem5.rs
+++ src/problems/problem5.rs
@@ -1,58 +1,94 @@
 use std::collections::HashMap;
+use std::ops::Range;
 
 use super::common::read_file;
 
 pub fn part1() {
+    do_work(&Mode::Problem1);
+}
+
+pub fn part2() {
+    do_work(&Mode::Problem2);
+}
+
+fn do_work(mode: &Mode) {
     let path = "test_files/5.txt".to_string();
     let lines = read_file(path);
 
     let mut counts: HashMap<(u32, u32), u32> = HashMap::new();
 
-    for line in lines{
-        let coords = create_line(&line);
+    for line in lines {
+        let coords = create_line(&line, mode);
         for c in coords {
             *counts.entry(c).or_insert(0) += 1;
         }
     }
     let mut passed_multiple_times: u32 = 0;
-    for (_, passes) in counts.iter(){
+    for (_, passes) in counts.iter() {
         if *passes > 1 {
             passed_multiple_times += 1;
         }
     }
 
     println!("Day 5");
-    println!("The number of overlapping points is {}", passed_multiple_times);
+    println!(
+        "The number of overlapping points is {}",
+        passed_multiple_times
+    );
 }
 
-pub fn part2() {
-    unimplemented!()
-}
-
-fn create_line(line: &str) -> Vec<(u32, u32)> {
+fn create_line(line: &str, mode: &Mode) -> Vec<(u32, u32)> {
     let coords: Vec<&str> = line.split(" -> ").collect();
-    let first_coord: Vec<u32> = coords[0]
+    let init: Vec<u32> = coords[0]
         .split(",")
         .map(|x| x.parse::<u32>().unwrap())
         .collect();
-    let second_coord: Vec<u32> = coords[1]
+    let end: Vec<u32> = coords[1]
         .split(",")
         .map(|x| x.parse::<u32>().unwrap())
         .collect();
 
-    let (changing_idx, static_idx) = if first_coord[0] == second_coord[0] {
+    let (changing_idx, static_idx) = if init[0] == end[0] {
         (1, 0)
-    } else if first_coord[1] == second_coord[1] {
+    } else if init[1] == end[1] {
         (0, 1)
     } else {
-        return vec!{};
+        return match *mode {
+            Mode::Problem1 => vec![],
+            Mode::Problem2 => create_diagonal((init[0], init[1]), (end[0], end[1])),
+        };
     };
-    let (bigger, smaller) = if first_coord[changing_idx] > second_coord[changing_idx] {
-        (first_coord[changing_idx], second_coord[changing_idx])
+    let (bigger, smaller) = if init[changing_idx] > end[changing_idx] {
+        (init[changing_idx], end[changing_idx])
     } else {
-        (second_coord[changing_idx], first_coord[changing_idx])
+        (end[changing_idx], init[changing_idx])
     };
     (smaller..bigger + 1)
-        .map(|x| if static_idx == 0 {(first_coord[0], x)} else {(x, first_coord[1])})
+        .map(|x| {
+            if static_idx == 0 {
+                (init[0], x)
+            } else {
+                (x, init[1])
+            }
+        })
         .collect::<Vec<(u32, u32)>>()
+}
+
+fn create_diagonal(start: (u32, u32), end: (u32, u32)) -> Vec<(u32, u32)> {
+    let range_x: Vec<u32> = if start.0 > end.0 {
+        (end.0..start.0 + 1).rev().collect()
+    } else {
+        (start.0..end.0 + 1).collect()
+    };
+    let range_y: Vec<u32> = if start.1 > end.1 {
+        (end.1..start.1 + 1).rev().collect()
+    } else {
+        (start.1..end.1 + 1).collect()
+    };
+    range_x.into_iter().zip(range_y).collect()
+}
+
+enum Mode {
+    Problem1,
+    Problem2,
 }
