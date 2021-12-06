723c31a3a5117cb4b274e9d7f9a5719e25f42365
Jacob Rivera <edjacob25@gmail.com>
2021-12-06 10:51:23 -0600

    Added second part of day 6

--- src/problems/problem6.rs
+++ src/problems/problem6.rs
@@ -21,7 +21,36 @@ pub fn part1() {
     }
 
     let num_of_fish = fishes.len();
+    println!("Day 6, part 1");
     println!("The number of fishes after 80 days is {}", num_of_fish);
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let path = "test_files/6.txt".to_string();
+    let lines = read_file(path);
+    let mut fishes: Vec<u64> = vec![0; 9];
+    let initial: Vec<usize> = lines[0]
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    for num in initial {
+        fishes[num] += 1;
+    }
+
+    for _ in 0..256 {
+        let mut new_pop = vec![0; 9];
+        for i in (0..9).rev() {
+            if i == 0 {
+                new_pop[6] += fishes[0];
+                new_pop[8] = fishes[0];
+            } else {
+                new_pop[i - 1] = fishes[i];
+            }
+        }
+        fishes = new_pop;
+    }
+    let num_of_fish: u64 = fishes.iter().sum();
+    println!("Day 6, part 1");
+    println!("The number of fishes after 80 days is {}", num_of_fish);
+}
