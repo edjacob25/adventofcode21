a1b2a7d45d13a964d2e6dc03eb73f5c473e745ce
Jacob Rivera <edjacob25@gmail.com>
2021-12-06 13:17:43 -0600

    Changed benchmarks to be groupped

--- benches/my_bench.rs
+++ benches/my_bench.rs
@@ -2,18 +2,35 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use advent2021::problems::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Day 1, part 1", |b| b.iter(|| problem1::part1()));
-    c.bench_function("Day 1, part 2", |b| b.iter(|| problem1::part2()));
-    c.bench_function("Day 2, part 1", |b| b.iter(|| problem2::part1()));
-    c.bench_function("Day 2, part 2", |b| b.iter(|| problem2::part2()));
-    c.bench_function("Day 3, part 1", |b| b.iter(|| problem3::part1()));
-    c.bench_function("Day 3, part 2", |b| b.iter(|| problem3::part2()));
-    c.bench_function("Day 4, part 1", |b| b.iter(|| problem4::part1()));
-    c.bench_function("Day 4, part 2", |b| b.iter(|| problem4::part2()));
-    c.bench_function("Day 5, part 1", |b| b.iter(|| problem5::part1()));
-    c.bench_function("Day 5, part 2", |b| b.iter(|| problem5::part2()));
-    c.bench_function("Day 6, part 1", |b| b.iter(|| problem6::part1()));
-    c.bench_function("Day 6, part 2", |b| b.iter(|| problem6::part2()));
+    let mut day = c.benchmark_group("Day 1");
+    day.bench_function("1", |b| b.iter(|| problem1::part1()));
+    day.bench_function("2", |b| b.iter(|| problem1::part2()));
+    day.finish();
+
+    let mut day = c.benchmark_group("Day 2");
+    day.bench_function("1", |b| b.iter(|| problem2::part1()));
+    day.bench_function("2", |b| b.iter(|| problem2::part2()));
+    day.finish();
+
+    let mut day = c.benchmark_group("Day 3");
+    day.bench_function("1", |b| b.iter(|| problem3::part1()));
+    day.bench_function("2", |b| b.iter(|| problem3::part2()));
+    day.finish();
+
+    let mut day = c.benchmark_group("Day 4");
+    day.bench_function("1", |b| b.iter(|| problem4::part1()));
+    day.bench_function("2", |b| b.iter(|| problem4::part2()));
+    day.finish();
+
+    let mut day = c.benchmark_group("Day 5");
+    day.bench_function("1", |b| b.iter(|| problem5::part1()));
+    day.bench_function("2", |b| b.iter(|| problem5::part2()));
+    day.finish();
+
+    let mut day = c.benchmark_group("Day 6");
+    day.bench_function("1", |b| b.iter(|| problem6::part1()));
+    day.bench_function("2", |b| b.iter(|| problem6::part2()));
+    day.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
