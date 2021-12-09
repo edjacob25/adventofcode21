4bf98d2e38aef4ebba88c42a4f69f4f32c7a9919
Jacob Rivera <edjacob25@gmail.com>
2021-12-09 13:55:24 -0600

    Added benchmarks for day 9

--- benches/my_bench.rs
+++ benches/my_bench.rs
@@ -41,6 +41,11 @@ fn criterion_benchmark(c: &mut Criterion) {
     day.bench_function("1", |b| b.iter(|| problem8::part1()));
     day.bench_function("2", |b| b.iter(|| problem8::part2()));
     day.finish();
+
+    let mut day = c.benchmark_group("Day 9");
+    day.bench_function("1", |b| b.iter(|| problem9::part1()));
+    day.bench_function("2", |b| b.iter(|| problem9::part2()));
+    day.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
