90495dba6d4c4be97b37849759c7550f7119f6cc
Jacob Rivera <edjacob25@gmail.com>
2021-12-15 00:54:06 -0600

    Addes second part of day 14

--- src/problems/problem14.rs
+++ src/problems/problem14.rs
@@ -20,18 +20,25 @@ pub fn part1() -> usize {
 pub fn part2() -> usize {
     let path = "test_files/14.txt".to_string();
     let lines = read_file(path);
-    let (mut seq, rules) = create_seq_and_rules(&lines);
+    let (seq, rules) = create_seq_and_rules(&lines);
 
-    for i in 0..40 {
-        step(&mut seq, &rules);
-        println!("{}", i);
+    let mut pairs = HashMap::new();
+    for i in 0..seq.len() - 1 {
+        *pairs.entry((seq[i], seq[i+1])).or_insert(0usize) += 1;
     }
-    let mut counts = HashMap::new();
+
+    let mut individuals = HashMap::new();
     for c in seq {
-        *counts.entry(c).or_insert(0usize) += 1;
+        *individuals.entry(c).or_insert(0usize) += 1;
     }
 
-    counts.values().max().unwrap() - counts.values().min().unwrap()
+    for _ in 0..40 {
+        //println!("{:?}, {:?}", pairs, individuals);
+        step_alt(&mut pairs, &mut individuals, &rules);
+    }
+
+
+    individuals.values().max().unwrap() - individuals.values().min().unwrap()
 }
 
 fn create_seq_and_rules (lines: &[String]) -> (Vec<char>, HashMap<(char, char), char>){
@@ -57,5 +64,25 @@ fn step(sequence: &mut Vec<char>, rules: &HashMap<(char, char), char>) {
 
     for (i, &c) in to_insert.iter().enumerate(){
         sequence.insert(i*2+ 1,c);
+    }
+}
+
+fn step_alt(pairs: &mut HashMap<(char, char), usize>, individuals: &mut HashMap<char, usize>, rules: &HashMap<(char, char), char>) {
+    let mut to_add = Vec::new();
+    let mut to_remove = Vec::new();
+
+    for (item, count) in pairs.iter() {
+        let rule = *rules.get(item).unwrap();
+        to_add.push(((item.0, rule), *count));
+        to_add.push(((rule, item.1), *count));
+        to_remove.push((*item, *count));
+        *individuals.entry(rule).or_default() += *count;
+    }
+
+    for (place, new) in to_add {
+        *pairs.entry(place).or_default() += new;
+    }
+    for (place, new) in to_remove {
+        *pairs.entry(place).or_default() -= new;
     }
 }
